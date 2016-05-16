d48a6f99c6aa34ac51e8a6824fd02188be15f2f2
zyguo <bpzxgzy@126.com>
2016-05-16 18:41:23 +0800

    some update

---
+++ app/src/main/java/com/zyguo/voicenote/view/ItemView.java
@@ -0,0 +1,13 @@
+package com.zyguo.voicenote.view;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by zyguo on 2016/5/16.
+ */
+public class ItemView extends View{
+    public ItemView(Context context) {
+        super(context);
+    }
+}
